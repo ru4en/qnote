671ede8b436b5cf2436f759b635c4e0ee010adfa
ru4en <ru4en@debian>
2021-05-31 18:49:00 +0100

    your message

--- __main__.py
+++ __main__.py
@@ -8,34 +8,48 @@ home = expanduser('~')
 NoteLocation = os.path.join(home,'notes')
 quickNoteLocation = os.path.join(NoteLocation,'quickNotes')
 
+def clearConsole():
+    os.system('clear')
+    return
+
 def dateOrTime(value):
     rawTime = datetime.now()
     if value == 'time':
-        time = rawTime.strftime('%I:%M%p')
+        time = rawTime.strftime('%I:%M %p')
         return time
     elif value == 'date':
         date = rawTime.strftime('%d-%B-%Y')
         return date
 
+
+def printNote(noteLocation):
+    print('\nNote Location: {}\n'.format(noteLocation))
+    note = open(noteLocation, 'r')
+    notevar = note.read()
+    print (notevar)
+    note.close()
+
+
 def main(argv):
+    clearConsole()
     try:
         if argv[0] == '-l' or argv[0] == '--list':
+            print('\nIndex \t Folder Name')
             for num, folders in enumerate(os.listdir(NoteLocation)):
-                print('{} ------- {}'.format(num,folders))
-            openFolder = int(input(': '))
+                print('{} \t {}'.format(num,folders))
+            openFolder = int(input('\n: '))
+            clearConsole()
             currentLocation = os.path.join(NoteLocation,os.listdir(NoteLocation)[openFolder])
-            print(os.listdir(NoteLocation)[openFolder])
+            print('\nCurrent Location: {}'.format(currentLocation))
             notesInFile = len(os.listdir(currentLocation))
-            print('{} NOTES IN THIS FOLDER.'.format(notesInFile)) 
+            print('\n{} Note(s) In This Folder.'.format(notesInFile))
+            print('\nIndex \t Date Created')
             for num, file in enumerate(os.listdir(currentLocation)):
-                print('{} ------- {}'.format(num,file))
-            openNote = int(input(': '))
-            print(currentLocation)
+                print('{} \t {}'.format(num,file))
+            openNote = int(input('\n: '))
+            clearConsole()
             currentLocation = (os.path.join(NoteLocation, os.listdir(NoteLocation)[openFolder], os.listdir(currentLocation)[openNote]))
-            note = open(currentLocation, 'r')
-            notevar = note.read()
-            print (notevar)
-            note.close()
+            printNote(currentLocation)
 
         elif argv[0] == '-h' or argv[0] == '--help':
             print('\nUsage: note [options]')
@@ -50,15 +64,21 @@ def main(argv):
             print('note alpha 1 (amd64)')
 
         else:
-            print(argv)
-            fileLocation = ('{}/{}'.format(quickNoteLocation, dateOrTime('time')))
-            print(fileLocation)
+            fileLocation = ('{}/{}'.format(quickNoteLocation, dateOrTime('date')))
+            try:
+                newNote = open(fileLocation, 'a+')
+            except:
+                os.makedirs(quickNoteLocation)
+            fileLocation = ('{}/{}'.format(quickNoteLocation, dateOrTime('date')))
+            note = '{}:\t{}\n'.format(dateOrTime('time') ,' '.join(argv))
             newNote = open(fileLocation, 'a+')
-            newNote.write(' '.join(argv))
+            newNote.write(note)
             newNote.close()
+            print('~Todays Notes~')
+            printNote(fileLocation)
     except IOError:
-        os.makedirs(quickNoteLocation)
-
+        print("ERROR")
+        
 if __name__ == '__main__':
     try:
         main(sys.argv[1:])
